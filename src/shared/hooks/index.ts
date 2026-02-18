63106b02e759a578c1f240a9242f3c287bcb585b
kimui <kimui2267@gmail.com>
2026-02-18 21:13:14 +0900

    refactor: shared/hooks 모듈 통합 export 구성

---
+++ src/shared/hooks/index.ts
@@ -0,0 +1,8 @@
+export { useDebounce } from './use-debounce';
+export { usePagination } from './use-pagination';
+export { useIntersectionObserver } from './use-intersection-observer';
+export { useLocalStorage } from './use-local-storage';
+export { useMediaQuery, BREAKPOINTS } from './use-media-query';
+export { useClickOutside } from './use-click-outside';
+export { useScrollLock } from './use-scroll-lock';
+export { useCopyToClipboard } from './use-copy-to-clipboard';
