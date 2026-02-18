cd01a85832d07da0ba9c5c8547484fbadf1663b4
kimui <kimui2267@gmail.com>
2026-02-18 21:08:01 +0900

    chore: Jest 단위 테스트 환경 설정

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({ dir: './' });
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: ['<rootDir>/src/**/__tests__/**/*.test.ts?(x)'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/.next/'],
+};
+
+export default createJestConfig(config);
